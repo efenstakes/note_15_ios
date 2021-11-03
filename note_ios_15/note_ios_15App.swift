f64e80c2a4132920656e6af17a8fa841ebf686d0
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-11-03 22:26:59 +0300

    Initial Commit

---
+++ note_ios_15/note_ios_15App.swift
@@ -0,0 +1,17 @@
+//
+//  note_ios_15App.swift
+//  note_ios_15
+//
+//  Created by MAC on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct note_ios_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ note_ios_15Tests/note_ios_15Tests.swift
@@ -0,0 +1,33 @@
+//
+//  note_ios_15Tests.swift
+//  note_ios_15Tests
+//
+//  Created by MAC on 11/3/21.
+//
+
+import XCTest
+@testable import note_ios_15
+
+class note_ios_15Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
