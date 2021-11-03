0adcb3d448af30794d438ae70707f80efb927fbd
Efen Stakes <efenstakes101@gmail.com>
2021-11-03 23:38:53 +0300

    add note card views

--- note_ios_15/ContentView.swift
+++ note_ios_15/ContentView.swift
@@ -13,7 +13,11 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                 LazyVStack {
-                  
+                    
+                    ForEach(notes) { note in
+                        NoteCardView(note: note)
+                    }
+                    
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
@@ -31,7 +35,64 @@ struct ContentView: View {
     }
 }
 
-
+struct NoteCardView: View {
+    let note: Note
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            
+            // image
+            Image(note.image)
+                .resizable()
+                .frame(height: 260, alignment: .topLeading)
+            
+            
+            // details
+            HStack(alignment: .center) {
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(note.title)
+                        .font(.title2)
+                        .bold()
+                    
+                    HStack {
+                        Image(note.people[0].image)
+                            .resizable()
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .clipShape(Circle())
+                        Text(note.people[0].name)
+                            .font(.body)
+                            .fontWeight(.regular)
+                    }
+                }
+                
+                Spacer()
+                
+                Text("Add new")
+                    .font(.caption)
+                    .padding()
+                    .background(
+                        Color.black.opacity(0.9)
+                    )
+                    .cornerRadius(10)
+                
+            }
+            .padding()
+            .frame(height: 100, alignment: .topLeading)
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(note.color.opacity(0.6))
+            
+        }
+        .clipped()
+        .frame(height: 360, alignment: .center)
+        .frame(maxWidth: .infinity)
+        .cornerRadius(20)
+        .padding()
+        
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
