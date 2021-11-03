7032ec11f8bc4b8b72db097f3e455951405b3fec
Efen Stakes <efenstakes101@gmail.com>
2021-11-03 23:10:11 +0300

    add app navigation titles

--- note_ios_15/ContentView.swift
+++ note_ios_15/ContentView.swift
@@ -10,10 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                  
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading:
+                    Text("Notes")
+                    .font(.title)
+                    .bold(),
+                trailing:
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+            )
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
